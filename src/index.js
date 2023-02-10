bb5439d791590cee0705d98d8316f1132c8886b7
Graeme McNish <graememcnish@gmail.com>
2023-02-10 10:49:12 -0800

    having difficulty with forEach

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './less/index.less'
 
 // Your code goes here!
+console.log("testing one two threen")
+
+let contentSection = document.querySelector(".content-section");
+contentSection.addEventListener('mouseenter', (evt) => {
+    evt.target.style.backgroundColor = '#17A2B8';
+})
+// contentSection.addEventListener('click', (evt) => {
+//     evt.target.style.backgroundColor = '#FFEBCD';
+// })
+
+contentSection.addEventListener('mouseleave', (evt) => {
+    evt.target.style.backgroundColor = '#FFFFFF';
+
+})
+
+let navLink = document.querySelector(".nav-link")
+
+navLink.addEventListener('click', (evt) => {
+    evt.target.style.backgroundColor = '#212529';
+})
+
+let destination = document.querySelectorAll(".destination");
+
+function mouseOver(trgt) {
+    trgt.addEventListener('mouseover', (evt) => {
+        evt.target.style.backgroundColor = 'pink'
+    })
+}
+
+destination.forEach(mouseOver(destination))
+// destination.forEach()addEventListener("mouseover", (evt) => {
+//     evt.target.style.backgroundColor = 'pink';
+// }
+    
+// )
