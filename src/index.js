8d24bc4f826558afb5fbeba34d77f2ceb58cfd13
Graeme McNish <graememcnish@gmail.com>
2023-02-11 07:21:46 -0800

    finished with assitance from solution video

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './less/index.less'
 
 // Your code goes here!
-console.log("testing one two threen")
+console.log("testing one two three");
 
 let contentSection = document.querySelector(".content-section");
 contentSection.addEventListener('mouseenter', (evt) => {
@@ -24,15 +24,75 @@ navLink.addEventListener('click', (evt) => {
 
 let destination = document.querySelectorAll(".destination");
 
-function mouseOver(trgt) {
-    trgt.addEventListener('mouseover', (evt) => {
-        evt.target.style.backgroundColor = 'pink'
-    })
-}
-
-destination.forEach(mouseOver(destination))
-// destination.forEach()addEventListener("mouseover", (evt) => {
-//     evt.target.style.backgroundColor = 'pink';
+// function mouseOver(trgt) {
+//     trgt.addEventListener('mouseover', (evt) => {
+//         evt.target.style.backgroundColor = 'pink'
+//     })
 // }
+
+// destination.forEach(mouseOver(destination))
+// // destination.forEach()addEventListener("mouseover", (evt) => {
+// //     evt.target.style.backgroundColor = 'pink';
+// // }
     
-// )
+// // )
+
+// 1 - load 
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'READY TO GO!!'
+
+    // 2 - copy
+
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+        .then(text => {
+            heading.textContent += text
+        })
+    })
+
+    // 3 - click 
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+
+    // 4 - dblclick
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.outerHTML = ''
+    })
+
+    // 5 - keydown
+    window.addEventListener('keydown', evt => {
+        if (evt.key == 6) {
+            document.body.innerHTML = '<h1>YOU RAN ORDER 66</h1>'
+        }
+    })
+
+    // 6 - mousemove
+    document.body.addEventListener('mousemove', evt => {
+        const {clientX, clientY} = evt
+        // console.log(`mouse is at ${clientX}, ${clientY}`)
+    })
+
+    // 7 - mouseenter
+    // 8 - mouseleave
+    const destinations = document.querySelectorAll('.destination')
+    
+    for (let destination of destinations) {
+        destination.addEventListener('mouseenter', () => {
+            destination.style.fontWeight = 'bold'
+        })
+        destination.addEventListener('mouseleave', () => {
+            setTimeout(() => {
+                destination.style.fontWeight = 'initial'
+            }, 500)
+        })
+    }
+    const btn = document.querySelector('.btn');
+    btn.addEventListener("click", (evt) => {
+        console.log(evt)
+        evt.preventDefault()
+    })
+    
+} 
